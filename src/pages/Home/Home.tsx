9bfca9b7b201684980a863461071e4bc0e7c59c9
TsioryHenitsoa <tsioryhents@gmail.com>
2025-09-02 13:07:24 +0300

    Première page avec Tailwind installé et structure créé

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+// src/pages/Home/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Tongasoa ato amin'ny Home lessy ry bogosy eeh !</h1>
+    </div>
+  );
+};
+
+export default Home;
